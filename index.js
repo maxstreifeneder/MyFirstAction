30c2a9b648c7319dbe99ac2db8fd9d84e9ac1890
Maxi Streifeneder <maximilian.streifeneder@sap.com>
2021-01-30 13:40:35 +0100

    added log outputs

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { Toolkit } = require("actions-toolkit");
 
-const newIssue = await tools.github.issues.create({
-  ...tools.context.repo,
-  title: "New issue!",
-  body: "Hello Universe!",
-});
-tools.log.info("this is a log output");
-tools.log(newIssue);
-
 // Run your GitHub Action!
 Toolkit.run(async (tools) => {
   tools.exit.success("We did it!");
+
+  const newIssue = await tools.github.issues.create({
+    ...tools.context.repo,
+    title: "New issue!",
+    body: "Hello Universe!",
+  });
+  tools.log.info("this is a log output");
+  tools.log(newIssue);
 });
